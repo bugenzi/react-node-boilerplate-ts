772db3489657971555431b305c9bb05a269829f0
bugenzi <amarbugarin@outlook.com>
2021-09-13 16:16:38 +0200

    refactored tsconfig , made some seperations and implemented eslint with prettier

--- www/client/index.tsx
+++ www/client/index.tsx
@@ -1,10 +1,7 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-
- return ( <h1>React and node boilerplate using ts </h1>)
-};
+const App = () => <h1>React and node boilerplate using ts </h1>;
 
 ReactDOM.render(
   <React.StrictMode>
